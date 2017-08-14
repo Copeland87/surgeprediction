9ea8f14e55867ad2aad52b67f94d22734edc73df
Zac Copeland <zcopeland87@gmail.com>
2017-08-14 10:06:18 -0500

    thoughts

---
+++ assets/javascript/lyft.js
@@ -0,0 +1,17 @@
+
+    !function(e,t,n,o,l,i,a,c,d,r,u,y,f,m,b,s){var B=t.createElement('script');B.async=!0,
+    B.onload=function(){s!==o&&(e[s]=e[o],delete e[o]),b!==l&&(e[b]=e[l],delete e[l]);
+    var n={address:d,latitude:r,longitude:u,name:y};e[s].initialize({clientId:i,clientToken:a,
+    googleApiKey:c,location:n,objectName:s,parentElement:t.body}),e[b].initialize({clientId:i,
+    clientToken:a,location:n,objectName:b,onClick:function(){return e[s].open(),!1},
+    parentElement:f,theme:m})},B.src=n,f.insertBefore(B,f.childNodes[0])}(window,document,
+    'https://cdn.lyft.com/lyft-web-sdk/1.1.2/lyftWebButton.min.js','lyftWebModal',
+    'lyftWebButton','p-1-AzA71Kjf',
+    'tvjbdnjpKyIECwh4e7WpykPt+aPqWowk017Nkgz5aL+n631wZ3bqGN3MugCEk7jMM9NBhQWm0UbSFjJ9oUNcO7ve9X9aCKg4WCHhFJTrx9n3nKa9DnLG8aA=',
+    'AIzaSyCcFc_Epx8h090pEC12Trq3RGh8dOjwZw4',
+    '',
+    '','','',
+    document.getElementById('lyft-web-button-parent'),
+    'mulberry-light','lyftWebButton','lyftWebModal'
+    );
+ 
